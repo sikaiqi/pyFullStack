5bdcdf650bb9aa889c34188ed045c664e9d98973
sikaiqi <sikaiqi1992@163.com>
2018-06-25 22:56:25 +0800

    20180625,python practice

--- year2018/month06/day25/test.py
+++ year2018/month06/day25/test.py
@@ -1,1 +1,23 @@
 print("this is my first python program")
+
+#占位符的使用  %s  %d  %f
+name = input("Name:")
+age = int(input("age:"))
+job = input("job:")
+salary = input("salary:")
+
+if(salary.isdigit()):
+    salary = int(salary)
+# else:
+#     # print("must input digit")
+#     exit("must input digit")
+
+msg = '''
+-----info of %s -----
+Name:%s
+age:%d
+job:%s
+salary:%f
+''' %(name,name,age,job,salary)
+
+print(msg)
